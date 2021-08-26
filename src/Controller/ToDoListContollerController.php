67ef814817a0bdcdb63bb990b951f3ee429d59fb
Szymon99d <57864883+Szymon99d@users.noreply.github.com>
2021-08-26 20:38:48 +0200

    first controller & main template

---
+++ src/Controller/ToDoListContollerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToDoListContollerController extends AbstractController
+{
+    #[Route('/', name: 'main')]
+    public function index(): Response
+    {
+        return $this->render('ToDoList/index.html.twig', [
+            'controller_name' => 'ToDoListContollerController',
+        ]);
+    }
+}
