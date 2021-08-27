6638b18db9c08619f59ff116c4837e489a2977e3
Szymon99d <57864883+Szymon99d@users.noreply.github.com>
2021-08-27 14:53:16 +0200

    Updated and added new routes

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -5,9 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class ToDoListController extends AbstractController
 {
+    private $security;
+    
+    public function __construct(Security $sec)
+    {
+        $this->security = $sec;
+    }
+    
     #[Route('/', name: 'main')]
     public function index(): Response
     {
@@ -19,6 +27,16 @@ class ToDoListController extends AbstractController
     #[Route('/register', name: 'register')]
     public function register(): Response
     {  
+        return $this->render('ToDoList/index.html.twig', [
+            'controller_name' => 'ToDoListContollerController',
+        ]);
+    }
+
+    #[Route('/my-tasks', name: 'myTasks')]
+    public function myTasks(): Response
+    {  
+        if($this->security->getUser()===null)
+            return $this->redirectToRoute('app_login');
         return $this->render('ToDoList/index.html.twig', [
             'controller_name' => 'ToDoListContollerController',
         ]);
