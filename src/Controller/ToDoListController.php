cf0bdbae19cc5b486ebf9d279e596f57cff4336a
Szymon99d <57864883+Szymon99d@users.noreply.github.com>
2021-08-27 14:48:52 +0200

    Updated name of the fil

---
+++ src/Controller/ToDoListController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToDoListController extends AbstractController
+{
+    #[Route('/', name: 'main')]
+    public function index(): Response
+    {
+        return $this->render('ToDoList/index.html.twig', [
+            'controller_name' => 'ToDoListContollerController',
+        ]);
+    }
+
+    #[Route('/register', name: 'register')]
+    public function register(): Response
+    {  
+        return $this->render('ToDoList/index.html.twig', [
+            'controller_name' => 'ToDoListContollerController',
+        ]);
+    }
+}
